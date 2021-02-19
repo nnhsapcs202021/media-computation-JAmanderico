23cfd9155706b1d4c2477345f7ffb1de8f4762aa
Joey Manderico <jamanderico@stu.naperville203.org>
2021-02-19 10:33:12 -0600

    Started Activity 7 Mirroring Activities

--- Picture.java
+++ Picture.java
@@ -15,170 +15,322 @@ import java.util.List; // resolves problem with java.awt.List and java.util.List
  */
 public class Picture extends SimplePicture 
 {
-  ///////////////////// constructors //////////////////////////////////
-  
-  /**
-   * Constructor that takes no arguments 
-   */
-  public Picture ()
-  {
-    /* not needed but use it to show students the implicit call to super()
-     * child constructors always call a parent constructor 
+    ///////////////////// constructors //////////////////////////////////
+
+    /**
+     * Constructor that takes no arguments 
      */
-    super();  
-  }
-  
-  /**
-   * Constructor that takes a file name and creates the picture 
-   * @param fileName the name of the file to create the picture from
-   */
-  public Picture(String fileName)
-  {
-    // let the parent class handle this fileName
-    super(fileName);
-  }
-  
-  /**
-   * Constructor that takes the width and height
-   * @param height the height of the desired picture
-   * @param width the width of the desired picture
-   */
-  public Picture(int height, int width)
-  {
-    // let the parent class handle this width and height
-    super(width,height);
-  }
-  
-  /**
-   * Constructor that takes a picture and creates a 
-   * copy of that picture
-   * @param copyPicture the picture to copy
-   */
-  public Picture(Picture copyPicture)
-  {
-    // let the parent class do the copy
-    super(copyPicture);
-  }
-  
-  /**
-   * Constructor that takes a buffered image
-   * @param image the buffered image to use
-   */
-  public Picture(BufferedImage image)
-  {
-    super(image);
-  }
-  
-  ////////////////////// methods ///////////////////////////////////////
-  
-  /**
-   * Method to return a string with information about this picture.
-   * @return a string with information about the picture such as fileName,
-   * height and width.
-   */
-  public String toString()
-  {
-    String output = "Picture, filename " + getFileName() + 
-      " height " + getHeight() 
-      + " width " + getWidth();
-    return output;
+    public Picture ()
+    {
+        /* not needed but use it to show students the implicit call to super()
+         * child constructors always call a parent constructor 
+         */
+        super();  
+    }
+
+    /**
+     * Constructor that takes a file name and creates the picture 
+     * @param fileName the name of the file to create the picture from
+     */
+    public Picture(String fileName)
+    {
+        // let the parent class handle this fileName
+        super(fileName);
+    }
+
+    /**
+     * Constructor that takes the width and height
+     * @param height the height of the desired picture
+     * @param width the width of the desired picture
+     */
+    public Picture(int height, int width)
+    {
+        // let the parent class handle this width and height
+        super(width,height);
+    }
+
+    /**
+     * Constructor that takes a picture and creates a 
+     * copy of that picture
+     * @param copyPicture the picture to copy
+     */
+    public Picture(Picture copyPicture)
+    {
+        // let the parent class do the copy
+        super(copyPicture);
+    }
+
+    /**
+     * Constructor that takes a buffered image
+     * @param image the buffered image to use
+     */
+    public Picture(BufferedImage image)
+    {
+        super(image);
+    }
+
+    ////////////////////// methods ///////////////////////////////////////
+
+    /**
+     * Method to return a string with information about this picture.
+     * @return a string with information about the picture such as fileName,
+     * height and width.
+     */
+    public String toString()
+    {
+        String output = "Picture, filename " + getFileName() + 
+            " height " + getHeight() 
+            + " width " + getWidth();
+        return output;
+
+    }
+
+    /** Method to set the blue to 0 */
+    public void zeroBlue()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setBlue(0);
+            }
+        }
+    }
+
+    /**
+     * Method to set the red & green values to 0
+     */
+    public void keepOnlyBlue()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setRed(0);
+                pixelObj.setGreen(0);
+            }
+        }
+    }
     
-  }
-  
-  /** Method to set the blue to 0 */
-  public void zeroBlue()
-  {
-    Pixel[][] pixels = this.getPixels2D();
-    for (Pixel[] rowArray : pixels)
+    /**
+     * Method to negate all the pixels: 
+     *     set the red value to 255 minus the current red value, 
+     *     the green value to 255 minus the current green value,
+     *     and the blue value to 255 minus the current blue value.
+     */
+    public void negate()
     {
-      for (Pixel pixelObj : rowArray)
-      {
-        pixelObj.setBlue(0);
-      }
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                int newRed = 225 - pixelObj.getRed();
+                int newGreen = 225 - pixelObj.getGreen();
+                int newBlue = 225 - pixelObj.getBlue();
+                pixelObj.setRed(newRed);
+                pixelObj.setGreen(newGreen);
+                pixelObj.setBlue(newBlue);
+            }
+        }
     }
-  }
-  
-  /** Method that mirrors the picture around a 
-    * vertical mirror in the center of the picture
-    * from left to right */
-  public void mirrorVertical()
-  {
-    Pixel[][] pixels = this.getPixels2D();
-    Pixel leftPixel = null;
-    Pixel rightPixel = null;
-    int width = pixels[0].length;
-    for (int row = 0; row < pixels.length; row++)
-    {
-      for (int col = 0; col < width / 2; col++)
-      {
-        leftPixel = pixels[row][col];
-        rightPixel = pixels[row][width - 1 - col];
-        rightPixel.setColor(leftPixel.getColor());
-      }
-    } 
-  }
-  
-  /** Mirror just part of a picture of a temple */
-  public void mirrorTemple()
-  {
-    int mirrorPoint = 276;
-    Pixel leftPixel = null;
-    Pixel rightPixel = null;
-    int count = 0;
-    Pixel[][] pixels = this.getPixels2D();
     
-    // loop through the rows
-    for (int row = 27; row < 97; row++)
+    /**
+     * Method to convert the image to grayscale.
+         Set the red, green, and blue values 
+         to the average of the current red, green, and blue values 
+         (add all three values and divide by 3).
+     */
+    public void grayscale()
     {
-      // loop from 13 to just before the mirror point
-      for (int col = 13; col < mirrorPoint; col++)
-      {
-        
-        leftPixel = pixels[row][col];      
-        rightPixel = pixels[row]                       
-                         [mirrorPoint - col + mirrorPoint];
-        rightPixel.setColor(leftPixel.getColor());
-      }
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                long avg = Math.round(pixelObj.getAverage());
+                pixelObj.setRed( (int) avg);
+                pixelObj.setGreen( (int) avg);
+                pixelObj.setBlue( (int) avg);
+            }
+        }
     }
-  }
-  
-  
-  /** Method to show large changes in color 
-    * @param edgeDist the distance for finding edges
-    */
-  public void edgeDetection(int edgeDist)
-  {
-    Pixel leftPixel = null;
-    Pixel rightPixel = null;
-    Pixel[][] pixels = this.getPixels2D();
-    Color rightColor = null;
-    for (int row = 0; row < pixels.length; row++)
+    
+    /**
+     * Method to modify pixel colors in order to see the fish clearer in the water.jpg image
+     */
+    // public void fixUnderwater()
+    // {
+        // Pixel[][] pixels = this.getPixels2D();
+        // for (Pixel[] rowArray : pixels)
+        // {
+            // for (Pixel pixelObj : rowArray)
+            // {
+                // if ( pixelObj.getAlpha() > 112.5 )
+                // {
+                    // int startAlpha = pixelObj.getAlpha();
+                    // int addition = 0;
+                    // int verify;
+                    // while ( addition < 11 & verify < 255)
+                    // {
+                        // addition += 1;
+                        // verify = startAlpha + addition;
+                    // }
+                    // addition += pixelObj.getAlpha(); pixelObj.setAlpha(addition);
+                // } else if (pixelObj.getAlpha() <= 112.5)
+                // {
+                    // int addition = 0;
+                    // while ( addition < 11  )
+                    // {
+                        // addition += 1;
+                    // }
+                    // addition -= pixelObj.getAlpha(); pixelObj.setAlpha(addition);
+                // }
+            // }
+        // }
+    // }
+    
+    /** Method that mirrors the picture around a 
+     * vertical mirror in the center of the picture
+     * from right to left */
+    public void mirrorVerticalRightToLeft() // FIX
     {
-      for (int col = 0; 
-           col < pixels[0].length-1; col++)
-      {
-        leftPixel = pixels[row][col];
-        rightPixel = pixels[row][col+1];
-        rightColor = rightPixel.getColor();
-        if (leftPixel.colorDistance(rightColor) > 
-            edgeDist)
-          leftPixel.setColor(Color.BLACK);
-        else
-          leftPixel.setColor(Color.WHITE);
-      }
+        Pixel[][] pixels = this.getPixels2D();
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        int width = pixels[0].length;
+        for (int row = 0; row < pixels.length; row++)
+        {
+            for (int col = 0; col < width / 2; col++)   //scroll through both dimensions
+            {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[width - 1 - row][col];
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        } 
     }
-  }
-  
-  
-  /* Main method for testing - each class in Java can have a main 
-   * method 
-   */
-  public static void main(String[] args) 
-  {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.zeroBlue();
-    beach.explore();
-  }
-  
+
+    /** Method that mirrors the picture around a 
+     * vertical mirror in the center of the picture
+     * from top to bottom */
+    public void mirrorHorizontal()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        Pixel topPixel = null;
+        Pixel bottomPixel = null;
+        int width = pixels[0].length;
+        for (int row = 0; row < pixels.length / 2; row++)
+        {
+            for (int col = 0; col < width; col++)   //scroll through both dimensions
+            {
+                topPixel = pixels[row][col];
+                bottomPixel = pixels[pixels.length - 1 - row][col];
+                bottomPixel.setColor(topPixel.getColor());
+            }
+        } 
+    }
+    
+    /** Method that mirrors the picture around a 
+     * vertical mirror in the center of the picture
+     * from bottom to top */
+    public void mirrorHorizontalBotToTop()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        Pixel topPixel = null;
+        Pixel bottomPixel = null;
+        int width = pixels[0].length;
+        for (int row = pixels.length / 2; row > 0; row--)
+        {
+            for (int col = width; col > 0; col--)   //scroll through both dimensions
+            {
+                bottomPixel = pixels[row][col];
+                topPixel = pixels[pixels.length - 1 + row][col];
+                topPixel.setColor(bottomPixel.getColor());
+            }
+        } 
+    }
+
+    /** Method that mirrors the picture around a 
+     * vertical mirror in the center of the picture
+     * from left to right */
+    public void mirrorVertical()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        int width = pixels[0].length;
+        for (int row = 0; row < pixels.length; row++)
+        {
+            for (int col = 0; col < width / 2; col++)
+            {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[row][width - 1 - col];
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        } 
+    }
+
+    /** Mirror just part of a picture of a temple */
+    public void mirrorTemple()
+    {
+        int mirrorPoint = 276;
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        int count = 0;
+        Pixel[][] pixels = this.getPixels2D();
+
+        // loop through the rows
+        for (int row = 27; row < 97; row++)
+        {
+            // loop from 13 to just before the mirror point
+            for (int col = 13; col < mirrorPoint; col++)
+            {
+
+                leftPixel = pixels[row][col];      
+                rightPixel = pixels[row]                       
+                [mirrorPoint - col + mirrorPoint];
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        }
+    }
+
+    /** Method to show large changes in color 
+     * @param edgeDist the distance for finding edges
+     */
+    public void edgeDetection(int edgeDist)
+    {
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        Pixel[][] pixels = this.getPixels2D();
+        Color rightColor = null;
+        for (int row = 0; row < pixels.length; row++)
+        {
+            for (int col = 0; 
+            col < pixels[0].length-1; col++)
+            {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[row][col+1];
+                rightColor = rightPixel.getColor();
+                if (leftPixel.colorDistance(rightColor) > 
+                edgeDist)
+                    leftPixel.setColor(Color.BLACK);
+                else
+                    leftPixel.setColor(Color.WHITE);
+            }
+        }
+    }
+
+    /* Main method for testing - each class in Java can have a main 
+     * method 
+     */
+    public static void main(String[] args) 
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.zeroBlue();
+        beach.explore();
+    }
+
 } // this } is the end of class Picture, put all new methods before this
--- PictureTester.java
+++ PictureTester.java
@@ -8,69 +8,140 @@
  */
 public class PictureTester
 {
-  /** Method to test zeroBlue */
-  public static void testZeroBlue()
-  {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.zeroBlue();
-    beach.explore();
-  }
-  
-  /** Method to test mirrorVertical */
-  public static void testMirrorVertical()
-  {
-    Picture caterpillar = new Picture("caterpillar.jpg");
-    caterpillar.explore();
-    caterpillar.mirrorVertical();
-    caterpillar.explore();
-  }
-  
-  /** Method to test mirrorTemple */
-  public static void testMirrorTemple()
-  {
-    Picture temple = new Picture("temple.jpg");
-    temple.explore();
-    temple.mirrorTemple();
-    temple.explore();
-  }
-  
-  /** Method to test edgeDetection */
-  public static void testEdgeDetection()
-  {
-    Picture swan = new Picture("swan.jpg");
-    swan.edgeDetection(10);
-    swan.explore();
-  }
-  
-  /** Main method for testing.  Every class can have a main
-    * method in Java */
-  public static void main(String[] args)
-  {
-    // uncomment a call here to run a test
-    // and comment out the ones you don't want
-    // to run
-    testZeroBlue();
-    //testKeepOnlyBlue();
-    //testKeepOnlyRed();
-    //testKeepOnlyGreen();
-    //testNegate();
-    //testGrayscale();
-    //testFixUnderwater();
-    //testMirrorVertical();
-    //testMirrorTemple();
-    //testMirrorArms();
-    //testMirrorGull();
-    //testMirrorDiagonal();
-    //testCollage();
-    //testCopy();
-    //testEdgeDetection();
-    //testEdgeDetection2();
-    //testChromakey();
-    //testEncodeAndDecode();
-    //testGetCountRedOverValue(250);
-    //testSetRedToHalfValueInTopHalf();
-    //testClearBlueOverValue(200);
-    //testGetAverageForColumn(0);
-  }
+    /** Method to test zeroBlue */
+    public static void testZeroBlue()
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.zeroBlue();
+        beach.explore();
+    }
+
+    /** Method to test mirrorVertical */
+    public static void testMirrorVertical()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorVertical();
+        caterpillar.explore();
+    }
+
+    /** Method to test mirrorTemple */
+    public static void testMirrorTemple()
+    {
+        Picture temple = new Picture("temple.jpg");
+        temple.explore();
+        temple.mirrorTemple();
+        temple.explore();
+    }
+
+    /** Method to test edgeDetection */
+    public static void testEdgeDetection()
+    {
+        Picture swan = new Picture("swan.jpg");
+        swan.edgeDetection(10);
+        swan.explore();
+    }
+
+    /**
+     * Method to test the keepOnlyBlue() method from Picture Class.
+     */
+    public static void testKeepOnlyBlue()
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.keepOnlyBlue();
+        beach.explore();
+    }
+    
+    /**
+     * Method to test the negate() method from Picture Class.
+     */
+    public static void testNegate()
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.negate();
+        beach.explore();
+    }
+    
+    /**
+     * Method to test the grayscale() method from Picture Class.
+     */
+    public static void testGrayscale()
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.grayscale();
+        beach.explore();
+    }
+
+    /**
+     * Method to test the fixUnderwater() method from the Picture class.
+     */
+    // public static void testFixUnderwater()
+    // {
+        // Picture underwater = new Picture("water.jpg");
+        // underwater.explore();
+        // underwater.fixUnderwater();
+        // underwater.explore();
+    // }
+    
+    public static void testMirrorVerticalRightToLeft()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorVertical();
+        caterpillar.explore();
+    }
+    
+    public static void testMirrorHorizontal()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorHorizontal();
+        caterpillar.explore();
+    }
+    
+    public static void testMirrorHorizontalBotToTop()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorHorizontalBotToTop();
+        caterpillar.explore();
+    }
+    
+    /** Main method for testing.  Every class can have a main
+     * method in Java */
+    public static void main(String[] args)
+    {
+        // uncomment a call here to run a test
+        // and comment out the ones you don't want
+        // to run
+        //testZeroBlue();
+        //testKeepOnlyBlue();
+        //testKeepOnlyRed();
+        //testKeepOnlyGreen();
+        //testNegate();
+        //testGrayscale();
+        //testFixUnderwater();
+        //testMirrorVertical();
+        //testMirrorVerticalRightToLeft();
+        //testMirrorHorizontal();
+        testMirrorHorizontalBotToTop();
+        //testMirrorTemple();
+        //testMirrorArms();
+        //testMirrorGull();
+        //testMirrorDiagonal();
+        //testCollage();
+        //testCopy();
+        //testEdgeDetection();
+        //testEdgeDetection2();
+        //testChromakey();
+        //testEncodeAndDecode();
+        //testGetCountRedOverValue(250);
+        //testSetRedToHalfValueInTopHalf();
+        //testClearBlueOverValue(200);
+        //testGetAverageForColumn(0);
+    }
 }
